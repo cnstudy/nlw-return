e9536815129a02b11efe84e45c5de3c5acc7e047
cnstudy <cicerogoncalves87@gmail.com>
2022-05-04 00:28:32 -0300

    Commit inicial do Widget - NLW Return

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./src/**/*.tsx"],
+  theme: {
+    extend: {
+      colors: {
+        brend: {
+          500: '#8b5cf6',
+        }
+      }
+    },
+  },
+  plugins: [],
+}
